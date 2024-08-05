30296f74cf974d51d194b56a270a5ff150db4054
Sebokaa <samuelthinks@gmail.com>
2024-08-04 21:49:55 -0400

    my changes

---
+++ app/Components/Snacks.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import "./Snacks.css"
+
+function Snacks() {
+  return (
+    <div className="foodContainer">
+      <div className="cardsList">
+      <div className="card">
+          <div className="picture">
+            <img src="https://pngimg.com/d/potato_chips_PNG79.png" alt="" />
+          </div>
+          <div className="title">
+            <h3>Potato Chips</h3>
+          </div>
+          <div className="quantity">
+            <button>-</button>
+            <h2>5</h2>
+            <button>+</button>
+          </div>
+        </div>
+        <div className="card">
+          <div className="picture">
+            <img src="https://pngimg.com/d/potato_chips_PNG79.png" alt="" />
+          </div>
+          <div className="title">
+            <h3>Potato Chips</h3>
+          </div>
+          <div className="quantity">
+            <button>-</button>
+            <h2>5</h2>
+            <button>+</button>
+          </div>
+        </div>
+        <div className="card">
+          <div className="picture">
+            <img src="https://pngimg.com/d/potato_chips_PNG79.png" alt="" />
+          </div>
+          <div className="title">
+            <h3>Potato Chips</h3>
+          </div>
+          <div className="quantity">
+            <button>-</button>
+            <h2>5</h2>
+            <button>+</button>
+          </div>
+        </div>
+        <div className="card">
+          <div className="picture">
+            <img src="https://pngimg.com/d/potato_chips_PNG79.png" alt="" />
+          </div>
+          <div className="title">
+            <h3>Potato Chips</h3>
+          </div>
+          <div className="quantity">
+            <button>-</button>
+            <h2>5</h2>
+            <button>+</button>
+          </div>
+        </div>
+        <div className="card">
+          <div className="picture">
+            <img src="https://pngimg.com/d/potato_chips_PNG79.png" alt="" />
+          </div>
+          <div className="title">
+            <h3>Potato Chips</h3>
+          </div>
+          <div className="quantity">
+            <button>-</button>
+            <h2>5</h2>
+            <button>+</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Snacks;
--- app/page.js
+++ app/page.js
@@ -1,95 +1,63 @@
+"use client";
 import Image from "next/image";
+import Snacks from "./Components/Snacks";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className={styles.App}>
+      <video
+        src={require("./videos/City.mp4")}
+        autoPlay
+        muted
+        loop
+        className={styles.bgVideo}
+      />
+      <div className={styles.navBar}>
+        <div className={styles.addItem}>
+          <input placeHolder="Add Item..." type="text" />
+          <button>
+            <svg
+              stroke="currentColor"
+              fill="currentColor"
+              stroke-width="0"
+              viewBox="0 0 16 16"
+              height="1em"
+              width="1em"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M14.002 2h-12a1 1 0 00-1 1v10a1 1 0 001 1h12a1 1 0 001-1V3a1 1 0 00-1-1zm-12-1a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V3a2 2 0 00-2-2h-12z"
+                clip-rule="evenodd"
+              ></path>
+              <path d="M10.648 7.646a.5.5 0 01.577-.093L15.002 9.5V14h-14v-2l2.646-2.354a.5.5 0 01.63-.062l2.66 1.773 3.71-3.71z"></path>
+              <path
+                fill-rule="evenodd"
+                d="M4.502 7a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </button>
+          <button>Add</button>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className={styles.application}>
+        <div className={styles.left}>
+          <div className={styles.sideBar}>
+            <h1>Pantry Tracker</h1>
+            <hr />
+            <h2>Snacks</h2>
+            <h2>Grains</h2>
+            <h2>Canned Goods</h2>
+            <h2>Baking Supplies</h2>
+            <h2>Oils and Condiments</h2>
+          </div>
+        </div>
+        <div className={styles.right}>
+          <Snacks />
+        </div>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
